0a9f52338bf04e292b6b99c268f105ce1aa29be7
unknown <faisalwn83@students.amikom.ac.id>
2023-03-21 18:18:41 +0700

    y

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Taxi taxi = new Taxi();
+            Taxi taxi1 = new Taxi();
+
+            taxi.drivername = "Wahyu";
+            taxi.onduty = true;
+            taxi.numpass = 33445;
+
+            taxi1.drivername = "Faisal";
+            taxi1.onduty = true;
+            taxi1.numpass = 11223;
+
+            taxi.info();
+            taxi.ongoing();
+            taxi.done();
+
+            taxi1.info();
+            taxi1.ongoing();
+            taxi1.done();
+
+            Console.ReadKey();
+        }
+    }
+}
